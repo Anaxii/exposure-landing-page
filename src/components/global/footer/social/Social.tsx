3a17e85d4a3bed7ce50a0f8e563842066886c208
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-09-06 15:55:46 -0700

    social links

--- src/components/global/footer/social/Social.tsx
+++ src/components/global/footer/social/Social.tsx
@@ -8,9 +8,14 @@ function Social() {
       <h4 className="text-primary-800 font-semibold text-base md:text-md">Join the Community</h4>
       <div className="flex items-center gap-3">
         {social.map(({ id, icon, url }) => (
-          <Link key={id} to={url}>
+            <a
+            key={id}
+            href={url}
+            target={"_blank"}
+            >
             <img className="w-6 md:w-9" src={icon} alt="" />
-          </Link>
+
+            </a>
         ))}
       </div>
     </div>
